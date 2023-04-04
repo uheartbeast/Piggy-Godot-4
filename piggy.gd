efc8e0e9e3737d93cfa6fd3de7e274ac49a89473
Heartbeast <heartbeast.studios@gmail.com>
2023-04-04 14:35:27 -0600

    Animating a Sprite + Apple Scene

--- piggy.gd
+++ piggy.gd
@@ -1,7 +1,17 @@
 extends Area2D
 
-var speed = 2
+@export var speed = 70
+
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
 
 func _process(delta):
 	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	position += input_vector * speed
+	
+	if input_vector == Vector2.ZERO:
+		animation_player.play("idle")
+	else:
+		animation_player.play("run")
+		if input_vector.x != 0:
+			sprite_2d.scale.x = sign(input_vector.x)
+	position += input_vector * speed * delta
